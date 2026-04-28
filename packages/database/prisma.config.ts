07500032929ff2345d696a68b17d50160c3e4b35
Khang0609 <trannguyenkhang836@gmail.com>
2026-04-28 20:21:04 +0700

    fix: Prisma!!

--- packages/database/prisma.config.ts
+++ packages/database/prisma.config.ts
@@ -1,12 +1,9 @@
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
-  migrations: {
-    path: "./prisma/migrations",
-  },
   datasource: {
-    url: process.env.DIRECT_URL,  // dùng DIRECT_URL cho CLI/migrate
+    url: env("DATABASE_URL"),
   },
 });
